b9c654a4933edaa367e5ab19a3d6f034ef866916
Carlos Mafra <mafra.projex@gmail.com>
2025-01-31 14:33:31 -0400

    [FEAT] - Teste

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'home';
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import {loadRemoteModule} from '@angular-architects/native-federation';
+
+export const routes: Routes = [{
+  path: 'microfrontend',
+  loadComponent: () => loadRemoteModule('mfe1','./Component').then((m) => m.AppComponent),
+}];
